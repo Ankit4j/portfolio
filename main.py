5598869e7d70605d366b950f7e39b858dec8949a
Ankit Jaiswal <ankitjais232@gmail.com>
2023-06-21 18:18:59 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+projects = [{'title': 'First Project',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'image_url': '../static/first-img.jpg'
+            },
+           {'title': 'Second Project',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'image_url': '../static/second-img.jpg'
+            },
+           {'title': 'Third Project',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'image_url': '../static/third-img.jpg'
+    }
+]
+
+
+
+@app.route("/")
+def get_home():
+    return render_template("home.html", projects = projects)
+
+@app.route("/projects")
+def get_projects():
+    return jsonify(projects)
+
+if __name__ == "__main__":
+    app.run(debug=True)
