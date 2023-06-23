f2ebadd853f1623830934ecedf402e4228a0b0d5
Ankit Jaiswal <ankitjais232@gmail.com>
2023-06-23 20:55:48 +0530

    done some code refactoring

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ app = Flask(__name__)
 
 projects = [{'title': 'First Project',
             'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            'image_url': '../static/first-img.jpg'
+            'image_url': '../static/first-img.jpg',
+            'project_url': 'https://github.com/Ankit4j/web-scraper'
             },
            {'title': 'Second Project',
             'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            'image_url': '../static/second-img.jpg'
+            'image_url': '../static/second-img.jpg',
+            'project_url': 'https://github.com/Ankit4j/chrome-extension'
             },
            {'title': 'Third Project',
             'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            'image_url': '../static/third-img.jpg'
+            'image_url': '../static/third-img.jpg',
+            'project_url': 'https://github.com/Ankit4j/drum-kit'
     }
 ]
 
